db6c08999966afa310ca02f07f5b11616c126b42
nidhiyerpude <nidhi.yerpude11@gmail.com>
2026-06-22 20:07:40 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -170,8 +170,7 @@ if st.button("Predict Resale Price"):
 
         st.subheader("AI Generated Resale Description")
         st.write(description)
-
-     except Exception as e:
+except Exception as e:
          st.warning("Gemini description could not be generated.")
 
     
